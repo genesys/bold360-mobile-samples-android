9bdd0255bbd1cdbe809b7bc5752b25bf3002c0c4
Tehila Rozin <tehila.rozin@logmein.com>
2019-05-12 19:36:55 +0300

    [BLD-11761]-gradle imports upgrade

--- Android/SDKSamples/app/src/main/java/com/sdk/samples/topics/BotChat.kt
+++ Android/SDKSamples/app/src/main/java/com/sdk/samples/topics/BotChat.kt
@@ -5,6 +5,12 @@ import com.nanorep.nanoengine.BotAccount
 
 class BotChat : BasicChat() {
     override fun getAccount(): Account {
+        /*return BotAccount(
+            "8bad6dea-8da4-4679-a23f-b10e62c84de8",
+            "jio",
+            "Staging_Updated",
+            "qa07"
+        )*/
         return BotAccount(
             "",
             "nanorep",
