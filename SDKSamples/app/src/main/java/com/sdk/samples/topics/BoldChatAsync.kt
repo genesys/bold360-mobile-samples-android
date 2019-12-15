ca2156dea25d4ebaa0ff0288df8952001c31794d
oz <oz.benoved@logmein.com>
2019-12-15 15:53:26 +0200

    [BLD-24659] - updated the UserInfo

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/BoldChatAsync.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BoldChatAsync.kt
@@ -10,10 +10,10 @@ open class BoldChatAsync : BasicChat() {
     protected val account:AsyncAccount by lazy {
 
         val userInfo = UserInfo("1234567654321234567")
-        userInfo.firstName = "fame"
-        userInfo.lastName = "s"
-        userInfo.email = "android@is.s"
-        userInfo.phoneNumber = "09666"
+        userInfo.firstName = "First name"
+        userInfo.lastName = "Last name"
+        userInfo.email = "Email@Bold.com"
+        userInfo.phoneNumber = "123456"
 
         AsyncAccount("2307475884:2403340045369405:KCxHNTjbS7qDY3CVmg0Z5jqHIIceg85X:alphawd2", "mobile12345").apply {
             getInfo().userInfo = userInfo
