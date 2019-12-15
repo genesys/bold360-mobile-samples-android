9fd6aed52b8b3092708b28ca754b8d3010adf60f
oz <oz.benoved@logmein.com>
2019-12-15 16:20:15 +0200

    [BLD-24659] - updated the imports of the SDK versions

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/BoldChatAsync.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BoldChatAsync.kt
@@ -9,17 +9,18 @@ open class BoldChatAsync : BasicChat() {
 
     protected val account:AsyncAccount by lazy {
 
-        val userInfo = UserInfo("1234567654321234567")
-        userInfo.firstName = "First name"
-        userInfo.lastName = "Last name"
-        userInfo.email = "Email@Bold.com"
-        userInfo.phoneNumber = "123456"
-
         AsyncAccount("2307475884:2403340045369405:KCxHNTjbS7qDY3CVmg0Z5jqHIIceg85X:alphawd2", "mobile12345").apply {
-            getInfo().userInfo = userInfo
-        }
 
+            info.userInfo = UserInfo("1234567654321234569").apply {
+                firstName = "First name"
+                lastName = "Last name"
+                email = "Email@Bold.com"
+                phoneNumber = "123456"
+            }
+
+        }
     }
+
     @JvmName("account") get
 
     override fun getAccount(): Account {
