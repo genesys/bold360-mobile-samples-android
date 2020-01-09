3de62cd16c30ac6c4d97146e12ddfdaa532245a1
oz <oz.benoved@logmein.com>
2020-01-09 12:25:07 +0200

    [BLD-24947] - fixed a crash at the BasicChat

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/BasicChat.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BasicChat.kt
@@ -9,6 +9,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.integration.core.StateEvent
 import com.nanorep.convesationui.structure.FriendlyDatestampFormatFactory
 import com.nanorep.convesationui.structure.controller.ChatController
@@ -22,6 +23,7 @@ import com.nanorep.sdkcore.utils.toast
 import com.sdk.samples.R
 import kotlinx.android.synthetic.main.activity_bot_chat.*
 import kotlinx.android.synthetic.main.restore_layout.*
+import kotlinx.coroutines.launch
 import kotlin.properties.Delegates
 
 abstract class BasicChat : AppCompatActivity(), ChatEventListener {
@@ -101,13 +103,13 @@ abstract class BasicChat : AppCompatActivity(), ChatEventListener {
         Log.d("Chat event", "chat in state: ${stateEvent.state}")
         when (stateEvent.state) {
             StateEvent.ChatWindowDetached -> finish()
-            StateEvent.Unavailable -> toast(this@BasicChat, stateEvent.state, Toast.LENGTH_SHORT, ColorDrawable(Color.GRAY))
+            StateEvent.Unavailable -> lifecycleScope.launch { toast(this@BasicChat, stateEvent.state, Toast.LENGTH_SHORT, ColorDrawable(Color.GRAY)) }
         }
     }
 
     override fun onError(error: NRError) {
         super.onError(error)
-        toast(this@BasicChat, error.toString(), Toast.LENGTH_SHORT, ColorDrawable(Color.GRAY))
+        lifecycleScope.launch { toast(this@BasicChat, error.toString(), Toast.LENGTH_SHORT, ColorDrawable(Color.GRAY)) }
     }
 
     override fun onBackPressed() {
