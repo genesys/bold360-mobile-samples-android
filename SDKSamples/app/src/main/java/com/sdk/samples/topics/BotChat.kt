508b531887701b1767780f7365cb639d93912667
oz <oz.benoved@logmein.com>
2020-03-10 12:07:47 +0200

    [BLD-28912] - disabled the upload button from being shown at the BotChat

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/BotChat.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BotChat.kt
@@ -1,5 +1,7 @@
 package com.sdk.samples.topics
 
+import com.nanorep.convesationui.structure.controller.ChatController
+import com.nanorep.convesationui.structure.providers.ChatUIProvider
 import com.nanorep.nanoengine.Account
 import com.nanorep.nanoengine.bot.BotAccount
 
@@ -14,13 +16,19 @@ open class BotChat : BasicChat() {
         return account
     }
 
+    override fun getBuilder(): ChatController.Builder {
+        return super.getBuilder().apply {
+            chatUIProvider(ChatUIProvider(this@BotChat).apply {
+                chatInputUIProvider.uiConfig.showUpload = false
+            })
+        }
+    }
+
     companion object{
 
         val formalBotAccount = BotAccount(
-            "",
-            "nanorep",
-            "English",
-            "" //https://eu1-1.nanorep.com/console/login.html
+            "", "jio",
+            "Staging_Updated", "mobilestaging", null
         )
 
         val testAccount = BotAccount(
