5d834f52af713bccc0410ba0ad975679952d0e53
Tehila Rozin <tehila.rozin@logmein.com>
2020-05-21 17:11:09 +0300

    [BLD-33286]-adding TTSReadAlterProvider to the Voice to Voice sample

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/BotVocToVocChat.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BotVocToVocChat.kt
@@ -1,15 +1,27 @@
 package com.sdk.samples.topics
 
-import com.nanorep.nanoengine.Account
-import com.nanorep.nanoengine.bot.BotAccount
+import com.nanorep.convesationui.structure.components.ReadRequest
+import com.nanorep.convesationui.structure.components.TTSReadAlterProvider
+import com.nanorep.convesationui.structure.controller.ChatController
 import com.nanorep.nanoengine.model.configuration.ConversationSettings
 import com.nanorep.nanoengine.model.configuration.VoiceSettings
 import com.nanorep.nanoengine.model.configuration.VoiceSupport
-import com.nanorep.sdkcore.utils.toast
 
 open class BotVocToVocChat : BotChat() {
 
+    private val readAlterProvider:TTSReadAlterProvider = object : TTSReadAlterProvider{
+        override fun alter(readRequest: ReadRequest, callback: (ReadRequest) -> Unit) {
+            readRequest.text = "Changing element text for testing: ${readRequest.readReqId}: original message: ${readRequest.text}"
+            callback.invoke(readRequest)
+        }
+    }
+
     override fun createChatSettings(): ConversationSettings {
         return super.createChatSettings().voiceSettings(VoiceSettings(VoiceSupport.VoiceToVoice))
+    }
+
+    override fun getBuilder(): ChatController.Builder {
+        return super.getBuilder()
+            .ttsReadAlterProvider(readAlterProvider) // Comment this to disable the read alter provider
     }
 }
