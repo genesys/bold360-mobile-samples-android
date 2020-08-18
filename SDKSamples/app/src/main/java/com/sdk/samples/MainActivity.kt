ba42d3b043f69288f809c10c066b42fff0a8b471
oz <oz.benoved@logmein.com>
2020-08-18 17:51:32 +0300

    [BLD-38525] - added custom form sample

--- SDKSamples/app/src/main/java/com/sdk/samples/MainActivity.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/MainActivity.kt
@@ -91,6 +91,10 @@ class MainActivity : AppCompatActivity() {
                 "com.sdk.sample.action.AUTOCOMPLETE",
                 getString(R.string.standalone_autocomplete),
                 ContextCompat.getDrawable(this, R.drawable.outline_text_format_black_24)
+            ), SampleTopic(
+                "com.sdk.sample.action.CUSTOM_FORM",
+                getString(R.string.custom_form),
+                ContextCompat.getDrawable(this, R.drawable.baseline_description_black_24)
             )
         )
 
---
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BoldCustomChatForm.kt
@@ -0,0 +1,36 @@
+package com.sdk.samples.topics
+
+import androidx.annotation.NonNull
+import com.integration.bold.boldchat.core.FormData
+import com.nanorep.convesationui.bold.ui.FormListener
+import com.nanorep.convesationui.structure.controller.ChatController
+import com.nanorep.convesationui.structure.controller.FormProvider
+import com.sdk.samples.R
+import com.sdk.samples.topics.extra.FormDummy
+
+const val FORM_DUMMY_FRAGMENT_TAG = "form_dummy_fragment"
+
+class BoldCustomChatForm : BotChat(){
+
+    inner class FormProviderSample: FormProvider {
+        override fun presentForm(formData: FormData, @NonNull callback: FormListener) {
+
+            // Demo implementation that presents present a dummy form :
+            val fragment = FormDummy.create(formData, callback)
+
+            supportFragmentManager.beginTransaction().replace(
+                R.id.chat_view,
+                fragment,
+                FORM_DUMMY_FRAGMENT_TAG).commit()
+        }
+    }
+
+    override fun getBuilder(): ChatController.Builder {
+
+        val settings = createChatSettings()
+
+        return ChatController.Builder(this)
+            .conversationSettings(settings)
+            .formProvider(FormProviderSample())
+    }
+}
