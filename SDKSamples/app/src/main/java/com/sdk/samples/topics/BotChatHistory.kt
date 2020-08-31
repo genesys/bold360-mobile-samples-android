06fe27feeb07104fcde5ed96ef44433c236332fb
Tehila Rozin <tehila.rozin@logmein.com>
2020-08-31 12:54:46 +0300

    [BLD-38825]-updating samples with history migration tool activation

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/BotChatHistory.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/BotChatHistory.kt
@@ -1,9 +1,12 @@
 package com.sdk.samples.topics
 
+import android.os.Bundle
+import com.nanorep.convesationui.utils.HistoryMigration.Companion.start
 import com.nanorep.nanoengine.Account
 import com.nanorep.nanoengine.bot.BotAccount
 import com.nanorep.sdkcore.utils.toast
 import com.sdk.samples.topics.extra.withId
+import com.sdk.samples.topics.history.HistoryMigrationProvider
 
 open class BotChatHistory : History() {
 
@@ -20,4 +23,15 @@ open class BotChatHistory : History() {
         toast(this@BotChatHistory, "The file upload action is not available for this sample.")
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        start(HistoryMigrationProvider(this){
+            runOnUiThread { super.startChat()}
+        })
+    }
+
+    override fun startChat() {
+
+    }
 }
