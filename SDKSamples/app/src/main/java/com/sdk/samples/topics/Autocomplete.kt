ed89aa8f728a2bb2095079c72c71635b18b2075b
oz <oz.benoved@logmein.com>
2021-07-01 14:34:48 +0300

    [BLD-46898] - fixed Autocomplete sample

--- SDKSamples/app/src/main/java/com/sdk/samples/topics/Autocomplete.kt
+++ SDKSamples/app/src/main/java/com/sdk/samples/topics/Autocomplete.kt
@@ -88,7 +88,7 @@ class Autocomplete : SampleActivity<AutocompleteActivityBinding>() {
      * some visible action on article selection: we display the fetched article body in a "WebView"
      */
     private fun onArticle(article: Article) {
-        var html = "<html>${ArticleFragment.STYLE_TO_HANDLE_TABLES}<body>${article.content
+        var html = "<html><Style>${ArticleFragment.STYLE_TO_HANDLE_TABLES}</Style><body>${article.content
             ?: "Not Available"}</body></html>"
         html = LinkedArticleHandler.updateLinkedArticles(html)
         binding.articleView.loadData(html, "text/html", "UTF-8")
