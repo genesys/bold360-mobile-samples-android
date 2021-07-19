c6096bbda817934da84eedcd225525fa9d103242
Tehila Rozin <tehila.rozin@logmein.com>
2021-07-19 16:00:31 +0300

    working with updated messaging lib + some related changes

--- SDKSamples/genesyshandover/src/main/java/com/bold360/genesyshandover/GenHandover.kt
+++ SDKSamples/genesyshandover/src/main/java/com/bold360/genesyshandover/GenHandover.kt
@@ -2,21 +2,25 @@ package com.bold360.genesyshandover
 
 import android.content.Context
 import android.util.Log
-import com.genesys.cloud.messenger.io.Configuration
-import com.genesys.cloud.messenger.io.MessagingClient
-import com.genesys.cloud.messenger.io.shyrka.receive.ErrorCodes
-import com.genesys.cloud.messenger.io.shyrka.receive.ErrorCodes.ATTACHMENT_NOT_SUCCESSFULLY_UPLOADED
-import com.genesys.cloud.messenger.io.shyrka.receive.MessageType
-import com.genesys.cloud.messenger.io.shyrka.receive.SessionResponse
-import com.genesys.cloud.messenger.io.shyrka.receive.StructuredMessage
-import com.genesys.cloud.messenger.io.shyrka.receive.WebMessagingMessage
+import com.genesys.cloud.messenger.transport.Configuration
+import com.genesys.cloud.messenger.transport.MessagingClient
+import com.genesys.cloud.messenger.transport.shyrka.receive.ErrorCodes
+import com.genesys.cloud.messenger.transport.shyrka.receive.MessageEntityList
+import com.genesys.cloud.messenger.transport.shyrka.receive.MessageType
+import com.genesys.cloud.messenger.transport.shyrka.receive.SessionResponse
+import com.genesys.cloud.messenger.transport.shyrka.receive.StructuredMessage
+import com.genesys.cloud.messenger.transport.shyrka.receive.WebMessagingMessage
+import com.genesys.cloud.messenger.transport.util.MobileMessenger
+import com.integration.core.ErrorEvent
 import com.integration.core.State
 import com.integration.core.StateEvent
 import com.integration.core.StateEvent.Companion.Created
 import com.integration.core.StateEvent.Companion.Ended
+import com.integration.core.StateEvent.Companion.Ending
 import com.integration.core.StateEvent.Companion.Preparing
 import com.integration.core.StateEvent.Companion.Started
 import com.nanorep.convesationui.structure.AccountListenerEvent
+import com.nanorep.convesationui.structure.HandoverAccount
 import com.nanorep.convesationui.structure.HandoverHandler
 import com.nanorep.convesationui.views.autocomplete.ChatInputData
 import com.nanorep.nanoengine.Account
@@ -39,9 +43,6 @@ import com.nanorep.sdkcore.utils.NRError
 import com.nanorep.sdkcore.utils.SystemUtil
 import com.nanorep.sdkcore.utils.lazyM
 import com.nanorep.sdkcore.utils.log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.util.UUID
 import java.util.concurrent.ConcurrentLinkedQueue
 
 
@@ -50,11 +51,13 @@ import java.util.concurrent.ConcurrentLinkedQueue
 *
 * */
 
-class GenAccount(token: String = UUID.randomUUID().toString()) : Account(token) {
+class GenAccount : Account("") {
 
-    var configuration: Configuration by lazyM {
-        DefaultConfig
-    }
+    var configuration: Configuration
+        get() = info.getConfiguration("clientConfiguration") ?: DefaultConfig
+        set(value) {
+            info.addConfigurations("clientConfiguration" to value)
+        }
 
     var sessionId: String?
         get() = info.getConfiguration("sessionId")
@@ -92,8 +95,28 @@ class GenAccount(token: String = UUID.randomUUID().toString()) : Account(token) 
         val DefaultConfig = Configuration(
             deploymentId = "f8aad9d7-f8e7-48e9-ab02-eef92bc4fd2f",
             messagingBaseAddress = "wss://webmessaging.inindca.com",
-            apiBaseAddress = "https://api.inindca.com"
+            apiBaseAddress = "https://api.inindca.com",
+            tokenStoreKey = "com.genesys.messenger.poc", // will be used to save the random UUID on the device shared preferences
+            logging = true
         )
+
+        /**
+         * Due to HandoverAccount not open to extension on this SDK version
+         */
+        fun toHandoverAccount(account: AccountInfo) : HandoverAccount {
+            return (account as? HandoverAccount) ?: HandoverAccount("").apply {
+                this.info = account.getInfo()
+            }
+        }
+
+        /**
+         * Due to HandoverAccount not open to extension on this SDK version
+         */
+        fun toGenAccount(account: AccountInfo) : GenAccount {
+            return (account as? GenAccount) ?: GenAccount().apply {
+                this.info = account.getInfo()
+            }
+        }
     }
 }
 
@@ -161,7 +184,7 @@ class MessagesQueue : ConcurrentLinkedQueue<GenHandover.OutgoingMessage>() {
     // only peek idles can be sent
 }
 
-class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
+class GenHandover(context: Context) :
     HandoverHandler(context) {
 
     class OutgoingMessage(var message: ChatStatement) {
@@ -220,18 +243,34 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
         }
     }
 
+    private var genAccount: GenAccount = GenAccount()
+        set(value) {
+            field = value
 
-    private var client = MessagingClient(
-        genAccount.configuration
-        /*configuration = Configuration(
-            deploymentId = "f8aad9d7-f8e7-48e9-ab02-eef92bc4fd2f",
-            messagingBaseAddress = "wss://webmessaging.inindca.com",
-            apiBaseAddress = "https://api.inindca.com"
-        )*/
-    )
+            context?.let {
+                client = try {
+                    MobileMessenger.createMessagingClient(
+                        context = it,
+                        configuration = field.configuration
+
+                    )
+                } catch (ex: Exception) {
+                    Log.e("GenHandover", "failed to create MessagingClient: ")
+                    ex.printStackTrace()
+                    passEvent(ErrorEvent(NRError(ex)))
+
+                    dummyMessagingClient
+                }
+            }
+        }
+
+    private var client: MessagingClient by lazyM {
+        dummyMessagingClient
+    }
+
 
     override val isActive: Boolean
-        get() = chatStarted //&& genAccount.sessionId != null sessionId is null on new session
+        get() = chatStarted && client.currentState == MessagingClient.State.CONNECTED//&& genAccount.sessionId != null sessionId is null on new session
 
 
     private val activeSession: MessagingClient?
@@ -243,22 +282,21 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
         return StatementScope.HandoverScope
     }
 
-
     override fun endChat(forceClose: Boolean) {
-        if(isActive){
+        if (isActive) {
             try {
                 client.disconnect()
+                passStateEvent(StateEvent(Ending, getScope()))
+
             } catch (t: Throwable) {
                 Log.e(TAG, "Failed to disconnect Genesys chat", t)
-            } finally {
-                injectSystemMessage("Chat ended")
             }
+        } else if (client.currentState == MessagingClient.State.CLOSED) {
+            passStateEvent(StateEvent(Ended, getScope()))
         }
 
-        handleEvent(State, StateEvent(Ended, getScope()))
-
-        enableChatInput(false, null);
         chatStarted = false
+        enableChatInput(false, null);
     }
 
 
@@ -289,8 +327,8 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
     override fun startChat(accountInfo: AccountInfo?) {
         super.startChat(accountInfo)
 
-        (accountInfo as? GenAccount)?.configuration?.let {
-            client = MessagingClient(it)
+        accountInfo?.let {
+            genAccount = GenAccount.toGenAccount(it)
         }
 
         client.stateListener = {
@@ -308,6 +346,8 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
 
         } catch (e: Throwable) {
             Log.e(TAG, "Failed to connect: state:${client.currentState} error: $e ")
+            injectSystemMessage("Failed to connect")
+            endChat(true)
         }
     }
 
@@ -327,6 +367,8 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
 
             else -> getError(message.code)?.let {
                 Log.w(TAG, "Got failure response")
+
+                passEvent(ErrorEvent(it))
             }//injectElement(SystemStatement("got: [${message.code}]"))
         }
 
@@ -343,19 +385,19 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
                         "Inbound" -> { // sent by user
                             messagesQueue.poll(StatusPending)?.let { outMessage ->
                                 val temp = outMessage.message.sId
-                                 msgBody.id?.let { outMessage.message.sId = it }
+                                msgBody.id?.let { outMessage.message.sId = it }
 
                                 // ?? how can we verify that the message we've got is the one we expect?
                                 //  messages can be sent from the server on malfunction, and we don't want to consider
                                 //  them unless it's our message
                                 Log.d(TAG, "handleMessage: polling user message from pending queue")
-                                updateElement(temp,  outMessage.message)
+                                updateElement(temp, outMessage.message)
                                 updateStatus(outMessage.message, StatusOk)
                             }
                         }
 
                         else -> {
-                            injectElement(IncomingStatement(msgBody.text?:"", scope = getScope()))
+                            injectElement(IncomingStatement(msgBody.text ?: "", scope = getScope()))
                         }
                     }
                 }
@@ -427,7 +469,7 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
 
     private fun getError(code: Int): NRError? {
         return when (code) {
-            in ErrorCodes.FEATURE_UNAVAILABLE.value..ATTACHMENT_NOT_SUCCESSFULLY_UPLOADED.value -> {
+            in ErrorCodes.FEATURE_UNAVAILABLE.value..ErrorCodes.ATTACHMENT_NOT_SUCCESSFULLY_UPLOADED.value -> {
                 NRError(NRError.GeneralError)
 
             }
@@ -452,8 +494,16 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
                 startConfiguredSession()
             }
 
+            MessagingClient.State.CLOSING -> {
+                if (chatStarted) {
+                    endChat()
+                }
+            }
+
             MessagingClient.State.CLOSED -> {
-                endChat()
+                injectSystemMessage("Chat ended")
+                handleEvent(State, StateEvent(Ended, getScope()))
+
             }
             else -> Log.d(TAG, "onStateChanged: unhandled state $state")
         }
@@ -462,7 +512,7 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
     private fun startConfiguredSession() {
         try {
             client.configureSession(
-                genAccount.apiKey, genAccount.email, genAccount.phoneNumber,
+                genAccount.email, genAccount.phoneNumber,
                 genAccount.firstName, genAccount.lastName
             )
         } catch (e: Throwable) {
@@ -498,5 +548,69 @@ class GenHandover(context: Context, val genAccount: GenAccount = GenAccount()) :
 
     companion object {
         const val TAG = "GenHandover"
+
+        val dummyMessagingClient = object : MessagingClient {
+            override val currentState: MessagingClient.State = MessagingClient.State.CLOSED
+            override var messageListener: ((message: WebMessagingMessage<*>) -> Unit)? = null
+            override var rawMessageListener: ((text: String) -> Unit)? = null
+            override val socketError: Throwable? = null
+            override var stateListener: ((MessagingClient.State) -> Unit)? = null
+            override val token: String = ""
+
+            override fun configureNewSession(
+                email: String?,
+                phoneNumber: String?,
+                firstName: String?,
+                lastName: String?
+            ) {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun configureSession(
+                email: String?,
+                phoneNumber: String?,
+                firstName: String?,
+                lastName: String?
+            ) {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun connect() {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun deleteAttachment(attachmentId: String) {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun disconnect() {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun generateDownloadUrl(attachmentId: String) {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun generateUploadUrl(
+                fileName: String,
+                fileType: String,
+                attachmentId: String?
+            ) {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override suspend fun getMessages(pageNumber: Int, pageSize: Int): MessageEntityList {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+                return MessageEntityList(pageSize = 0, pageNumber = 0, total = 0, pageCount = 0)
+            }
+
+            override fun sendHealthCheck() {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+
+            override fun sendMessage(text: String) {
+                Log.e("DummyMessagingClient", "Action can't be done. restart sample")
+            }
+        }
     }
 }
--- SDKSamples/genesyshandover/src/main/java/com/bold360/genesyshandover/MainActivity.kt
+++ SDKSamples/genesyshandover/src/main/java/com/bold360/genesyshandover/MainActivity.kt
@@ -4,7 +4,8 @@ import com.common.topicsbase.History
 import com.common.utils.chatForm.FormFieldFactory
 import com.common.utils.chatForm.defs.ChatType
 import com.common.utils.chatForm.defs.DataKeys
-import com.genesys.cloud.messenger.io.Configuration
+import com.genesys.cloud.messenger.transport.Configuration
+import com.google.gson.JsonPrimitive
 import com.integration.core.StateEvent
 import com.nanorep.convesationui.structure.AccountListenerEvent
 import com.nanorep.convesationui.structure.HandoverHandler
@@ -26,7 +27,7 @@ class MainActivity : History() {
 
     override fun getChatBuilder(): ChatController.Builder? {
         return super.getChatBuilder()?.apply {
-            chatHandoverHandler(GenHandover(this@MainActivity, genAccount))
+            chatHandoverHandler(GenHandover(this@MainActivity))
             accountProvider(accountProvider)
         }
     }
@@ -34,12 +35,24 @@ class MainActivity : History() {
     override val extraDataFields: (() -> List<FormFieldFactory.FormField>)?
         get() = {
             (super.extraDataFields?.invoke()?.toMutableList() ?: mutableListOf()).apply {
+
+                add(FormFieldFactory.TextField( ChatType.Bot, "Messaging Chat details:"))
+                // adding genesys chat data:
+
                 add(FormFieldFactory.TextInputField( ChatType.Bot, "deploymentId", GenAccount.DefaultConfig.deploymentId,
                     "Deployment id", false ))
                 add(FormFieldFactory.TextInputField( ChatType.Bot, "messagingBaseAddress", GenAccount.DefaultConfig.messagingBaseAddress,
                     "Messaging base address", false ))
                 add(FormFieldFactory.TextInputField( ChatType.Bot, "apiBaseAddress", GenAccount.DefaultConfig.apiBaseAddress,
                     "API base address", false ))
+                add(FormFieldFactory.TextInputField( ChatType.Bot, "userFirstName", "",
+                    "User first name", false ))
+                add(FormFieldFactory.TextInputField( ChatType.Bot, "userLastName", "",
+                    "User last name", false ))
+                add(FormFieldFactory.TextInputField( ChatType.Bot, "userEmail", "",
+                    "User email", false ))
+                /*add(FormFieldFactory.Option( ChatType.ChatSelection, DataKeys.ChatTypeKey, ChatType.ContinueLast ))
+                        add(FormFieldFactory.RestoreSwitch(false))*/
             }
 
         }
@@ -48,29 +61,38 @@ class MainActivity : History() {
 
         sampleFormViewModel.sampleData.value?.account?.let{
             genAccount.apply {
-                val deploymentId = it["deploymentId"]?.toString() ?: configuration.deploymentId
-                val messagingBaseAddress = it["messagingBaseAddress"]?.toString() ?: configuration.messagingBaseAddress
-                val apiBaseAddress = it["apiBaseAddress"]?.toString() ?: configuration.apiBaseAddress
-                configuration = Configuration(deploymentId, messagingBaseAddress, apiBaseAddress)
+                val deploymentId = (it["deploymentId"] as? JsonPrimitive)?.asString ?: configuration.deploymentId
+                val messagingBaseAddress = (it["messagingBaseAddress"] as? JsonPrimitive)?.asString ?: configuration.messagingBaseAddress
+                val apiBaseAddress = (it["apiBaseAddress"] as? JsonPrimitive)?.asString ?: configuration.apiBaseAddress
+                configuration = Configuration(deploymentId, messagingBaseAddress, apiBaseAddress,
+                    getString(R.string.token_store_key))
 
-                firstName = "Android"
-                lastName = "Test"
-                email = "peter.parker@marvel.com"
+                firstName = (it["userFirstName"] as? JsonPrimitive)?.asString?: "Android"
+                lastName = (it["userLastName"] as? JsonPrimitive)?.asString ?: "Test"
+                email = (it["userEmail"] as? JsonPrimitive)?.asString ?: "peter.parker@marvel.com"
             }
         }
 
         return super.prepareAccount()
     }
 
+    override fun onAccountDataReady() {
+        // prevents removal of account form, enables backing to it from the sample form
+    }
 
     inner class GenAccountProvider : AccountSessionListener {
         override fun provide(info: AccountInfo, callback: Completion<AccountInfo>) {
-            callback.onComplete(genAccount)
+            callback.onComplete(GenAccount.toHandoverAccount(genAccount))
         }
 
         override fun update(account: AccountInfo) {
-            //TODO check for session
+
         }
 
+        override fun onConfigUpdate(account: AccountInfo, updateKey: String, updatedValue: Any?) {
+            super.onConfigUpdate(account, updateKey, updatedValue)
+
+            //TODO check for session
+        }
     }
 }
