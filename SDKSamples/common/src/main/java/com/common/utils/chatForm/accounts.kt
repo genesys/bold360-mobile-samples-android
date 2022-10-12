8c5397ed9318ee7d938e4963e2e55a825007a4cc
Kalman Perkel <kalman.perkel@genesys.com>
2022-10-12 16:50:49 +0300

    develop_kalman - removed prefilled accounts data

--- SDKSamples/common/src/main/java/com/common/utils/chatForm/accounts.kt
+++ SDKSamples/common/src/main/java/com/common/utils/chatForm/accounts.kt
@@ -10,10 +10,10 @@ import java.util.UUID
 object Accounts {
 
     private val formalBoldAccount: BoldAccount
-        get() = BoldAccount("2300000001700000000:2278936004449775473:sHkdAhpSpMO/cnqzemsYUuf2iFOyPUYV") // Mobile
+        get() = BoldAccount("") // Mobile
 
     private val fameBoldAccount: BoldAccount
-        get() = BoldAccount("2300000001700000000:2279148490312878292:grCCPGyzmyITEocnaE+owvjtbasV16eV") // Fame
+        get() = BoldAccount("") // Fame
 
     val defaultBoldAccount: BoldAccount
         get() = fameBoldAccount
@@ -21,28 +21,28 @@ object Accounts {
     private val formalBotAccount: BotAccount
         get() = BotAccount(
             "",
-            "nanorep",
-            "English",
+            "",
+            "",
             "" //https://eu1-1.nanorep.com/console/login.html
         )
 
     private val testBotAccount: BotAccount
         get() = BotAccount(
-            "", "nanorep",
-            "English", "mobilestaging")
+            "", "",
+            "", "")
 
     val defaultBotAccount: BotAccount
         get() = testBotAccount
 
     val defaultAsyncAccount = AsyncAccount(
-        "2300000001700000000:2279533687831071375:MlVOftOF/UFUUqPPSbMSDAnQjITxOrQW:gamma",
-        "MobileAsyncStagingNew12345"
+        "",
+        ""
     ).apply {
         info.userInfo = UserInfo(UUID.randomUUID().toString()).apply {
-            firstName = "Android"
-            lastName = "Samples"
-            email = "android.samples@bold.com"
-            phoneNumber = "111-111-1111"
+            firstName = ""
+            lastName = ""
+            email = ""
+            phoneNumber = ""
         }
     }
 }
