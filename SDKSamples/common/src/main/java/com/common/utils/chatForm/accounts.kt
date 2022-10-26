554eccaa0a9a49f00fa1df474905078f73e09717
KalmanPerkelGenesys <113455277+KalmanPerkelGenesys@users.noreply.github.com>
2022-10-26 18:28:18 +0300

    Develop to master (#5)
    * Bold 4.8.3 Playstore version (#3)
    
    * Bold latest version
    
    * develop_kalman - app name changed to Genesys Bold Messenger
    
    * develop_kalman - added app icons
    
    * develop_kalman - change full demo app package name
    
    * develop_kalman - removed prefilled accounts data
    
    * develop_kalman - update application name
    
    * develop_kalman - update the sample to work with the latest Bold version 4.8.3
    
    * develop - removing unnecessary { changing = true }
    
    * develop - keep formalBoldAccount
    
    * develop - removing fameBoldAccount
    
    * develop - restore accounts
    
    * develop-to-master - update imports
    
    * develop-to-master - remove accounts
    
    * develop-to-master - code cleaning
    
    * develop-to-master - undo removal
    
    * develop-to-master - removing the dev commented imports

--- SDKSamples/common/src/main/java/com/common/utils/chatForm/accounts.kt
+++ SDKSamples/common/src/main/java/com/common/utils/chatForm/accounts.kt
@@ -10,7 +10,7 @@ import java.util.UUID
 object Accounts {
 
     private val formalBoldAccount: BoldAccount
-        get() = BoldAccount("2300000001700000000:2278936004449775473:sHkdAhpSpMO/cnqzemsYUuf2iFOyPUYV") // Mobile
+        get() = BoldAccount("") // Mobile
 
     val defaultBoldAccount: BoldAccount
         get() = formalBoldAccount
@@ -18,8 +18,8 @@ object Accounts {
     private val formalBotAccount: BotAccount
         get() = BotAccount(
             "",
-            "nanorep",
-            "English",
+            "",
+            "",
             "" //https://eu1-1.nanorep.com/console/login.html
         )
 
@@ -27,14 +27,14 @@ object Accounts {
         get() = formalBotAccount
 
     val defaultAsyncAccount = AsyncAccount(
-        "2300000001700000000:2279533687831071375:MlVOftOF/UFUUqPPSbMSDAnQjITxOrQW:gamma",
-        "MobileAsyncStagingNew12345"
+        "",
+        ""
     ).apply {
         info.userInfo = UserInfo(UUID.randomUUID().toString()).apply {
-            firstName = "Android"
-            lastName = "Samples"
-            email = "android.samples@bold.com"
-            phoneNumber = "111-111-1111"
+            firstName = ""
+            lastName = ""
+            email = ""
+            phoneNumber = ""
         }
     }
 }
